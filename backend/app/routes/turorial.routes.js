efd0ca6d0caa1685c3bbba726b2ba9d4e5dbe869
Yakshith K D <yakshithkd97@gmail.com>
2026-02-24 13:15:59 +0530

    project initiation

---
+++ backend/app/routes/turorial.routes.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+  const tutorials = require("../controllers/tutorial.controller");
+  const router = require("express").Router();
+
+  router.post("/", tutorials.create);
+  router.get("/", tutorials.findAll);
+  router.get("/:id", tutorials.findOne);
+  router.put("/:id", tutorials.update);
+  router.delete("/:id", tutorials.delete);
+
+  app.use("/api/tutorials", router);
+};
